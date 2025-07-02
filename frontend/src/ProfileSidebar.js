b6b1947a5cee8fe015c5c38492fa5a41ac3f21ab
eunsung <eunsung.shin@gmail.com>
2025-07-02 17:03:33 +0900

    feat: 프로필 사이드바 컴포넌트 및 스타일 추가

---
+++ frontend/src/ProfileSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProfileSidebar.css';
+
+export default function ProfileSidebar() {
+    return (
+        <div className="profile-sidebar">
+            <div className="avatar-circle">🧑</div>
+            <div className="profile-name">신은성</div>
+            <div className="profile-desc">AI 자기소개 챗봇</div>
+        </div>
+    );
+} 
